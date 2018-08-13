c039d87e9cac6ee656a09c475308a88453cb39f7
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-08-13 10:03:02 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
+require 'pry'
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    # binding.pry
+    # Student.create(params[:student])
+    Student.create(first_name:params[:student][:first_name], last_name:params[:student][:last_name])
+    # binding.pry
+    redirect_to students_path
   end
 
 end
